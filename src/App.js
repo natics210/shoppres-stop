58cfe4fffd9dd305291dfe52ada6c1111b46716f
nadeem.mohammed3@ibm.com <nadeem.mohammed3@ibm.com>
2021-01-05 11:39:45 +0530

    install react-router-dom package Added Hats Dummy Page Added BrowserRoutre in App.js for routing Update Directory page to pass other properties instead of single Update Menu-item component added withRouter and onclick function to navigate to other pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
+import { Route } from 'react-router-dom';
+ 
+const HatsPage = () => (
+    <div>
+        <h1>Hats Page</h1>
+    </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage}/>
+      <Route exact path="/shop/hats" component={HatsPage}/>
     </div>
   );
 }
