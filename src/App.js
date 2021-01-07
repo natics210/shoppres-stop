3efe50d72ee820ed9110524bbe3c3495339704ab
nadeem.mohammed3@ibm.com <nadeem.mohammed3@ibm.com>
2021-01-07 11:07:05 +0530

    Update App.scss added anchor style Update App.js import header component Added crown.svg

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import HomePage from './pages/home/homepage.component';
 import './App.css';
 import ShopPage from './pages/shop/shoppage.component';
+import Header from '../src/components/header/header.component';
 
 
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Route exact path="/" component={HomePage}/>
       <Route exact path="/shop" component={ShopPage}/>
     </div>
