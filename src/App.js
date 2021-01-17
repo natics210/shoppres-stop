6d033cbd6844c4259a637df44fb84f665f0b05d9
nadeem.mohammed3@ibm.com <nadeem.mohammed3@ibm.com>
2021-01-17 11:22:27 +0530

    Updating app route if user loggedIn redirect to Home instead of SignIn Added user.types.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './pages/home/homepage.component';
 import './App.css';
 import ShopPage from './pages/shop/shoppage.component';
@@ -43,14 +43,18 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/shop" component={ShopPage}/>
-          <Route exact path="/signIn" component={SignInAndSignUp}/>
+          <Route exact path="/signIn" render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUp />)}/>
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser : user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser : user => dispatch(setCurrentUser(user)) 
 })
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
